3c59445e2dcfc1c123780cbfc2fe45a6cd5915a0
nino <ninoJay@foxmail.com>
2023-11-23 20:43:37 +0800

    Update print-theme-info.js

--- scripts/events/print-theme-info.js
+++ scripts/events/print-theme-info.js
@@ -6,14 +6,16 @@ hexo.on('ready', () => {
   if (!/^(g|s)/.test(hexo.env.cmd)) return
   const { version } = require('../../package.json')
   hexo.log.info(`
--------------------------------------------------------
+
+Welcome.
+----------------------------------------------------------------
    ___  ____
   / _ |/ / /
  / __ /_  _/
-/_/ |_|/_/            Reading and writing.
---------------------------------------------------------
-The latest hexo-theme-A4 version is ${version}.
-url: https://github.com/HiNinoJay/hexo-theme-A4/releases
---------------------------------------------------------
+/_/ |_|/_/                                 Reading and writing.
+-----------------------------------------------------------------
+Your hexo-theme-A4 version is ${version}. Maybe it can be updated.
+The latest: https://github.com/HiNinoJay/hexo-theme-A4/releases
+-----------------------------------------------------------------
 `)
 })
