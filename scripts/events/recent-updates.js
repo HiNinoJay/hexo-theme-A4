df4f124e7410209993bce19564a7ed1c42052942
HiNinoJay <ninojay@foxmail.com>
2024-09-26 20:59:54 +0800

    🌕 最近更新模块

---
+++ scripts/events/recent-updates.js
@@ -0,0 +1,14 @@
+/**
+ * recent updates page
+*/
+
+hexo.extend.generator.register('recent-updates', function(locals){
+    return {
+      path: 'recent-updates.html',
+      layout: 'layout',
+      data: {
+        title: '最近更新列表',
+        page: locals.pages.findOne({path: 'recent-updates.html'})
+      }
+    }
+  });
